3d5208f54101347bececafd52b57cfea3d82233b
Shrek4495 <94527349+Shrek4495@users.noreply.github.com>
2023-02-27 00:07:43 +0500

    Delete answers_controller.rb

--- app/controllers/answers_controller.rb
+++
@@ -1,9 +0,0 @@
-class AnswersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :find_question, only: %i[new create]
-
-  private
-  def find_question
-    @question = Question.find(params[:question_id])
-  end
-end
