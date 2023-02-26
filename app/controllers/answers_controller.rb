7f1ac0dea71997444228b7cd101de4904ef926f0
shrek4495 <shrekloginov@gmail.com>
2023-02-27 00:02:11 +0500

    fck

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,9 @@
+class AnswersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_question, only: %i[new create]
+
+  private
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+end
