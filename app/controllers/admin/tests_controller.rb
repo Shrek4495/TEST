210225f36a3cb83e07b730081b23f26f2d05ae63
shrek4495 <shrekloginov@gmail.com>
2023-03-15 00:11:54 +0500

    Add devise and Admin

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,61 @@
+class Admin::TestsController < Admin::BaseController
+  # before_action :authenticate_user!
+  before_action :find_test, only: %i[destroy update show edit]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+  def index
+    @tests = Test.all
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = current_user.created_tests.new(test_params)
+
+    if @test.save
+      redirect_to admin_tests_path(@test)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
+  end
+
+  def show
+    @questions = @test.questions
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :category_id, :level)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    redirect_to admin_tests_path
+  end
+end
