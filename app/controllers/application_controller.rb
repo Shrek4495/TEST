726c58e49c920e3c18957a86fe9156d2d98178f2
shrek4495 <shrekloginov@gmail.com>
2023-03-15 00:11:06 +0500

    Add devise and Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_locale
 
   def after_sign_in_path_for(user)
     user.admin? ? admin_tests_path : tests_path
   end
-
-  def default_url_options
-    I18n.locale.eql?(I18n.default_locale) ? {} : { lang: I18n.locale }
-  end
-
-  private
-
-  def set_locale
-    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
-  end
-
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
   end
-
 end
