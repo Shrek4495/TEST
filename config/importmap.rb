a48eddc85fdff0b8f835913bfcf07a235b9e4522
shrek4495 <shrekloginov@gmail.com>
2023-03-16 12:10:36 +0500

    Add Locale and bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
