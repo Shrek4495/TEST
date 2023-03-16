698735d076c82367b951b3b2f2767236142f97d9
shrek4495 <shrekloginov@gmail.com>
2023-03-16 12:10:36 +0500

    Add Locale and bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require rails-ujs
+//= require activestorage
+//= require popper
+//= require bootstrap-sprockets
+import "popper"
+import "bootstrap"
+//= require_tree .
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-users = User.create([{name: 'Vasya'}, {name: 'Petya'}])
+Category.destroy_all
+Test.destroy_all
+Question.destroy_all
+Answer.destroy_all
+TestPassage.destroy_all
 
-categories = Category.create([{title: 'Ryby'}, {title: 'Python'}])
 
-tests = Test.create([{title: 'Руби массивы', level: 2, author: users.last, category: categories.first},
-                     {title: 'Python методы', level: 3, author: users.last, category: categories.last}])
-
-questions = Question.create([{body: ' Аббривиатура массива', test: tests.first},
-                             {body: 'Метод для вывода сообщения на экран', test: tests.last}])
-
-Answer.create([{body: 'Arr', correct: true, question: questions.first},
-               {body: 'print', correct: true, question: questions.last}])
-
-TestsUser.create(test: tests.last, users: users.last)
+categories = Category.create([{ title: 'Ruby' }, { title: 'Ruby on Rails' }])
+tests = Test.create([{ title: 'Основы Руби', author: users.last,
+                       category: categories.first },
+                     { title: 'Rails-модели', level: 6, author: users.last,
+                       category: categories.last }])
+questions = Question.create([{ body: 'Как вычисляется квадратный корень?',
+                               test: tests.first },
+                             { body: 'Какая команда генерирует модель?',
+                               test: tests.last },
+                             { body: 'Как добавить новый элемент в массив?',
+                               test: tests.first },
+                             { body: 'Что из перечисленного является коллбеком модели?',
+                               test: tests.last }])
+Answer.create([{ body: 'Math.sqrt', correct: true,
+                 question: questions[0] },
+               { body: 'Sqrt.math', correct: false,
+                 question: questions[0] },
+               { body: 'bin/rails g migration', correct: false,
+                 question: questions[1] },
+               { body: 'bin/rails g modulation', correct: false,
+                 question: questions[1] },
+               { body: 'bin/rails g mutation', correct: false,
+                 question: questions[1] },
+               { body: 'bin/rails g model', correct: true,
+                 question: questions[1] },
+               { body: 'array.push()', correct: true,
+                 question: questions[2] },
+               { body: 'array << something', correct: true,
+                 question: questions[2] },
+               { body: 'array <> something', correct: false,
+                 question: questions[2] },
+               { body: 'before_validation', correct: true,
+                 question: questions[3] },
+               { body: 'before congratulations', correct: false,
+                 question: questions[3] },
+               { body: 'after kill', correct: false,
+                 question: questions[3] },
+                 { body: 'after_commit', correct: true,
+                   question: questions[3] }])
